3855f54317db34a824c21d2424e27ac1bf05ba3f
Pavithra7771 <thrap5403@gmail.com>
2024-10-08 02:10:17 -0700

    new commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    
+<center>
+ <h1>PAVITHRA J G </h1>
+ <b>CONTACT</b>
+ <p>Address:Thottiyam,Trichy <br></br>
+ Email id:thrap5403@gmail.com<br></br>
+ Mobile:9715430875</p>
+ <h2>PROFESSIONAL PROFILE</h2>
+ <p>To purse a challenge career and be part of a progressive organization <br></br>that gives scope to enchance my knowledge skills and to reaches the pinnacle<br></br> in the programming and development field with sheer determination,dedication and hardwork.</p>
+ <h3>EXPERTISE SKILLS</h3>
+ <p>Git<br></br>Github<br></br>HTML<br></br>CSS</p>
+ <h4>LANGUAGES</h4>                             
+ <p>Tamil<br></br>English</p>     
+ <b>INTEREST</b>
+ <p>Travelling<br></br>Reading</p>
+ <b>EDUCATION</b>  
+ <p>Vetri vinayaha college of Engineering and Technology<br></br>Bachelor of Engineering in Computer science<br></br>Sowdaambikka Matric. Hr.Sec.School<br></br>HSC<br></br>Dr.G.V.N.Matriculation School<br></br>SSLC</p>
+ <b>WORKSHOPS AND INTERNSHIP</b>
+ <p>Workshop for Graphic Design<br></br>Internship for data Analytics<br></br>Internship for Web Development</p>
+</center>
+
+      
+
+
+
+    </div>
+  );
+}
+
+export default App;
